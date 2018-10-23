430bf71147cf12dad3ae9f224001c03e8a264b30
Charlie Tran <hi@charlietran.com>
2018-10-23 17:30:59 -0400

    Functional Tests in Docker
    * Add Firefox and Selenium to Docker environment
    
    * Add geckodriver binary to bin, modify PATH in Dockerfile to include bin folder

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,9 @@ WORKDIR /code
 ADD Pipfile /code/
 ADD Pipfile.lock /code/
 RUN pip install pipenv
-RUN pipenv install \ 
- --deploy --system --dev --ignore-pipfile
- # $(test "$DJANGO_ENV" == production || echo "--dev")
+RUN pipenv install \
+ --deploy --system --ignore-pipfile \
+  $([ "$DJANGO_ENV" = production ] || echo "--dev")
+RUN apt-get update && apt-get -y install firefox-esr
 ADD . /code/
+ENV PATH="${PATH}:/code/bin"
--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        options = Options()
+        options.headless=True
+        self.browser = webdriver.Firefox(options=options)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_sign_up(self):
+        # Emmy has heard about this Geph website that helps you
+        # schedule boardgames. She visits it in her browser
+        self.browser.get('http://web:8000')
+
+        # She notices the page is titled 'Geph'
+        self.assertIn('Geph', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main()
